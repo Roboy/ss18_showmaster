c7b1846a9e2353f627eb8d744c7c69ea3c5e65d7
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-03 12:17:48 +0200

    minor changes to script

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -4,6 +4,6 @@ clear
 
 #screen -S roboy
 
-python3 ../../../ParlAI/projects/convai2/baselines/profilememory/train.py -hs 1024 -nl 3 -lr 0.001 -dr 0.2 -att general >& ../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
+python3 ../../../ParlAI/projects/convai2/baselines/profilememory/train.py -hs 256 -nl 2 -lr 0.001 -dr 0.2 -att general -bs 32 >& ../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
 #| tee data/models/convai2/profilememory/profilememory.log
 # -esz 128
