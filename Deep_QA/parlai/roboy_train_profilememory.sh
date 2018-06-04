78c942cc8f3b14ca977ce6b77d4e76618b283856
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-04 18:01:29 +0200

    tuned hypers

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -4,6 +4,6 @@ clear
 
 #screen -S roboy
 
-python3 ../../../ParlAI/projects/convai2/baselines/profilememory/train.py -lr 0.01 -bs 32 >& ../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
+python3 ../../../ParlAI/projects/convai2/baselines/profilememory/train.py -lr 0.1 -bs 32 >& ../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
 #| tee data/models/convai2/profilememory/profilememory.log
 # -esz 128
