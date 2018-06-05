9b03b7eee4e6e99bc4aafdb089864606e64c3721
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-05 12:15:19 +0200

    truncation to smalles possible -> higher training efficiency

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -2,7 +2,7 @@
 # clear
 #screen -S roboy
 
-python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -lr 0.1 -dr 0.2 -tr 100 -dec same -opt adam -ltim 10 -vtim 600 -vme 1000 -vp 10 -vmt accuracy -vmm max -vcut 1.0 -dbf True -lfc False -stim 900 &>../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
+python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -lr 0.1 -dr 0.2 -tr -1 -dec same -opt adam -ltim 10 -vtim 600 -vme 1000 -vp 10 -vmt accuracy -vmm max -vcut 1.0 -dbf True -lfc False -stim 900 &>../../../ParlAI/data/models/convai2/profilememory/profilememory.txt
 
 ############################# Legend #############################
 ############################# Model #############################
