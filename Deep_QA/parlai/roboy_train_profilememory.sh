ba27e59c7095f5260c7b85f824c23189b32dc122
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-05 09:48:12 +0200

    anotehr tr

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -2,10 +2,9 @@
 # clear
 #screen -S roboy
 
-python3 ../../../ParlAI/examples/train_model.py \
+python3 ../../../ParlAI/examples/train_model.py -m 'projects.personachat.persona_seq2seq:PersonachatSeqseqAgentSplit'\
 ############################# Model #############################
 # Model
--m 'projects.personachat.persona_seq2seq:PersonachatSeqseqAgentSplit'\
 # Task: define personachat task
 -t 'personachat:self'\
 # Model file location
