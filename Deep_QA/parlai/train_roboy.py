261a71b40c2af8b0ba800b85bf4828704d34249a
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-05 11:01:14 +0200

    modiefied hypers

--- Deep_QA/parlai/train_roboy.py
+++ Deep_QA/parlai/train_roboy.py
@@ -34,7 +34,13 @@ if __name__ == '__main__':
         personachat_sharelt=True,
         personachat_learnreweight=True,
         personachat_reweight='use',
-        truncate=100,
+        #truncate=100, # Truncate (default 100, -1 gives minimum length):
+        #'truncate input & output lengths to speed up '
+        # training (may reduce accuracy). This fixes all '
+        # input and output to have a maximum length and to '
+        # be similar in length to one another by throwing '
+        # away extra tokens. This reduces the total amount '
+        # of padding in the batches.'
         rank_candidates=True,
                         #validation_every_n_secs=300,
         validation_metric='accuracy', #(default: f1)
