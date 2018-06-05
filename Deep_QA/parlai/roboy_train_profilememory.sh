48cc21a80fda53c0129450a723124f281c5dbc1a
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-05 11:03:59 +0200

    modiefied hypers

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # clear
-screen -S roboy
+#screen -S roboy
 
 python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -lr 0.01 -dr 0.2 -tr 100 -dec same -opt adam -ltim 10 -vtim 600 -vme 1000 -vp 10 -vmt accuracy -vmm max -vcut 1.0 -dbf True -lfc False 2>&1 | tee ../../../ParlAI/data/models/convai2/profilememory/profilememory_log.txt
 
