a52eef863d3382d01412e6dfee77ba941d7a3e0e
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-05 11:33:04 +0200

    fixed redirecting to file

--- Deep_QA/parlai/roboy_train_profilememory.sh
+++ Deep_QA/parlai/roboy_train_profilememory.sh
@@ -2,10 +2,8 @@
 # clear
 #screen -S roboy
 
-python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -lr 0.01 -dr 0.2 -tr 100 -dec same -opt adam -ltim 10 -vtim 600 -vme 1000 -vp 10 -vmt accuracy -vmm max -vcut 1.0 -dbf True -lfc False >& ../../../ParlAI/data/models/convai2/profilememory/_profilememory_log.txt
+python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -lr 0.01 -dr 0.2 -tr 100 -dec same -opt adam -ltim 10 -vtim 600 -vme 1000 -vp 10 -vmt accuracy -vmm max -vcut 1.0 -dbf True -lfc False &> ../../../ParlAI/data/models/convai2/profilememory/_profilememory_log.txt
 
-# 2>&1 | tee ../../../ParlAI/data/models/convai2/profilememory/_profilememory_log.txt
 
+# 2>&1 | tee ../../../ParlAI/data/models/convai2/profilememory/profilememory_log.txt
 # Logging
-#
-
