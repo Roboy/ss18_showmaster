2fa104cfaf863352e58fc2bdc521c9f8f21da2a9
Christoph <34374252+christqoh@users.noreply.github.com>
2018-06-15 16:07:19 +0200

    initialization

---
+++ Deep_QA/parlai/advertise_gnlp_service.py
@@ -0,0 +1,14 @@
+# /bin/bash
+export PYTHONPATH=
+export PATH=~/anaconda3/bin:$PATH
+
+source activate python35
+
+python gnlp_ros_srv.py --modelTag pretrainedv2 --test ros
+
+
+
+
+
+
+#python3 projects/convai2/baselines/profilememory/interactive.py
